5691dd123da367536d98e2cc928883469a5e63d1
danilevich23555 <danilevich23555@yandex.ru>
2021-10-17 09:34:30 +0300

    add metod averege and output str

--- main.py
+++ main.py
@@ -18,6 +18,20 @@ class Student:
                 lector.grades_lector[course] = [grade]
         else:
             return 'Ошибка'
+    def averege(self):
+        i = 0
+        b = 0
+        c = 0
+        a = list(self.grades.values())
+        while i < len(a):
+            b = b + sum(a[i])
+            c = c + len(a[i])
+            i = i + 1
+            return  (b/c)
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nCредняя оценка за лекции: {self.averege()}' \
+              f'\nКурсы в процесе изучения: {", ".join(self.courses_in_progress)}\nЗавершенные курсы: {self.finished_courses}'
+        return res
 
 
 class Mentor:
@@ -27,9 +41,25 @@ class Mentor:
         self.courses_attached = []
 
 class Lector(Mentor):
+    b = 0
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades_lector = {}
+
+    def averege(self):
+        i = 0
+        b = 0
+        c = 0
+        a = list(self.grades_lector.values())
+        while i < len(a):
+            b = b + sum(a[i])
+            c = c + len(a[i])
+            i = i + 1
+            return  (b/c)
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nCредняя оценка за лекции: {self.averege()}'
+        return res
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -42,27 +72,41 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}'
+        return res
 
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['PHP']
 
 
 
 cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
-print(cool_mentor.courses_attached)
+cool_mentor.courses_attached += ['PHP']
 cool_lector = Lector('Some', 'Buddy')
 cool_lector.courses_attached += ['Python']
+cool_lector.courses_attached += ['PHP']
 
-best_student.rate_hw_lector(cool_lector, 'Python', 7)
-best_student.rate_hw_lector(cool_lector, 'PHP', 8)
 
+best_student.rate_hw_lector(cool_lector, 'Python', 5)
+best_student.rate_hw_lector(cool_lector, 'Python', 5)
+best_student.rate_hw_lector(cool_lector, 'PHP', 5)
+
+cool_mentor.rate_hw_student(best_student, 'PHP', 10)
 cool_mentor.rate_hw_student(best_student, 'Python', 10)
 cool_mentor.rate_hw_student(best_student, 'Python', 10)
-cool_mentor.rate_hw_student(best_student, 'Python', 10)
 
-print(best_student.grades)
-print(cool_lector.grades_lector)
+
+
+
+
+
+print(cool_mentor)
+print('')
+print(cool_lector)
+print('')
+print(best_student)
